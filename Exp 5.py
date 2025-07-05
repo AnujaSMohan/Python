ae579d94030bae71033489ccf128e5651aaca168
AnujaSMohan <anujasmohan4@gmail.com>
2025-07-05 14:56:42 +0530

    Update Exp 5.py

--- Exp 5.py
+++ Exp 5.py
@@ -1,5 +1,5 @@
-print("Niranjana S Nair")
-print("Admission no : A24MCA047 ")
+print("Name : Anuja S Mohan ")
+print("Admission No: A24MCA016")
 print("Ascending & Descending Experiemnt : 5")
 fruits={'Mango': 3, 'Grapes' :2, 'Apple':4, 'Banana':1}
 
