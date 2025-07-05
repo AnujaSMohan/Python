ce3bf3c4e963589045ab6fd908424ef6a1b6b149
AnujaSMohan <anujasmohan4@gmail.com>
2025-07-05 14:56:56 +0530

    Update c2-Exp 3.py

--- c2-Exp 3.py
+++ c2-Exp 3.py
@@ -1,5 +1,5 @@
-print("Name : Niranjana S Nair")
-print("Admission no : A24MCA047")
+print("Name : Anuja S Mohan ")
+print("Admission No: A24MCA016")
 print("Experiment no : 3")
 def factor(n):
     return [i for i in range(1, n + 1) if n % i == 0]
