a63cf70ac06a6e672325b96f9cd7340d1d3f37e4
AnujaSMohan <anujasmohan4@gmail.com>
2025-07-05 14:53:55 +0530

    Update C2-Exp 2.py

--- C2-Exp 2.py
+++ C2-Exp 2.py
@@ -1,5 +1,5 @@
-print("Name : Niranjana S Nair")
-print("Admission no : A24MCA047")
+print("Name : Anuja S Mohan ")
+print("Admission No: A24MCA016")
 print("Experiment no : 2")
 def char_freq(s):
     frequency = {}
